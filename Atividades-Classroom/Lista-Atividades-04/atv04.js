e808ae6a01c4840ad101e78b056b9c3995d62730
Nemesio carlos <114183514+NemesioCarlos@users.noreply.github.com>
2023-07-25 16:53:25 -0300

    Add files via upload

---
+++ Atividades-Classroom/Lista-Atividades-04/atv04.js
@@ -0,0 +1,83 @@
+//1. Implemente o código do slide de número 5.
+
+]let phrase = prompt("Eu amo estudar")
+console.log(phrase)
+
+2. Implemente o código do slide de número 13.
+
+    let string ="123"
+    console.log(typeof Number(string))
+
+    let number = "321"
+    console.log(typeof String(string))
+
+
+3. Implemente o código do slide de número 16.
+
+    let number01 = prompt("Digite o primeiro número")
+    let number02 = prompt("Digite o segundo número")
+
+    let result = Number(number01) + Number(number02)
+    console.log(result)
+
+4. Implemente o código do slide de número 23.
+    string1 = "Estudar "
+    string2 = "É bom!"
+    console.log(string1 + string2)
+    number = 48
+    console.log(string1 + string2 + " " + number)
+
+5. Solicite ao usuário seu nome e imprima uma mensagem de boas-vindas na tela.
+    let user = prompt("Digite seu nome")
+    alert(`Bem-vindo ${user}`)
+
+6. Peça ao usuário que digite sua idade em texto (por exemplo, "18") e converta-a em um número
+inteiro.    
+    let AgeUser = prompt("Digite sua idade ")
+    console.log(typeof Number(AgeUser))
+
+7. Receba um número inteiro do usuário e converta-o em um número decimal (float).
+let ageUser = parseInt(prompt("Digite um numero inteiro para converter "))
+
+ageUser = parseFloat(ageUser)
+console.log(typeof (ageUser) )
+console.log(ageUser)
+
+8. Peça ao usuário para digitar dois números inteiros e exiba a soma deles.
+    let numb1 = parseInt(prompt("Digite um numero  "))
+    let numb2 = parseInt(prompt("Digite outro numero  "))
+    lert(`A soma dos numeros ${numb1} e ${numb2} é ${numb1 + numb2}`)
+
+9. Receba um número decimal do usuário e calcule o seu quadrado.
+    
+
+10. Peça ao usuário que insira o seu ano de nascimento e, em seguida, exiba a sua idade.
+
+
+11. Peça ao usuário que digite seu primeiro nome e seu sobrenome separadamente. Em seguida,
+concatene-os em uma única string e exiba o nome completo.
+
+
+12. Solicite ao usuário uma sequência de números separados por espaço e exiba quantos números
+foram digitados.
+
+
+13. Receba o nome de um animal digitado pelo usuário e exiba uma mensagem informando qual
+animal foi digitado.
+
+
+14. Peça ao usuário que digite o seu nome e o seu sobrenome. Em seguida, exiba o nome completo
+invertido (sobrenome, nome).
+
+
+15. Receba uma string digitada pelo usuário e imprima o seu tamanho (número de caracteres).
+
+
+16. Solicite ao usuário um número inteiro e exiba se ele é par ou ímpar.
+
+
+17. Receba um número inteiro digitado pelo usuário e verifique se ele é positivo ou negativo.
+
+
+18. Peça ao usuário que insira dois números e exiba o maior deles.
+
---
+++ Atividades-Classroom/Lista-Atividades-04/index.js
@@ -0,0 +1,3 @@
+let numb1 = parseFloat(prompt("Digite um numero  "))
+ let quadrado = numb1^2
+alert (quadrado)
