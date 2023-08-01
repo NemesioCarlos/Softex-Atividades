ea78ab6ed216efdeb0d6e368e75727cdb758c79b
Nemesio carlos <114183514+NemesioCarlos@users.noreply.github.com>
2023-08-01 16:58:04 -0300

    Add files via upload

---
+++ Atividades-Classroom/Lista-Atividades-05/testes.js
@@ -0,0 +1,6 @@
+let letter = prompt("Digite uma letra para saber se é vogal ou consoante")
+if ((letter === "a") || (letter === "e") || (letter === "i") || (letter === "o") || (letter === "u")) {
+    alert("É vogal")
+} else {
+    alert("É consoante")
+}
