485b0d36f92f1d5486d19856a5f80aee34171646
Nemesio carlos <114183514+NemesioCarlos@users.noreply.github.com>
2023-08-03 10:50:16 -0300

    Add files via upload

---
+++ Softex-Atividades-Modulo-03-Joyclass/Atividade-01-listas-JS/index.js
@@ -0,0 +1,47 @@
+let nameList = [
+  "Tales",
+  "Pitagoras",
+  "Socrates",
+  "Platão",
+  "Aristóteles",
+  "Sêneca",
+  "Epiteto",
+  "Kant",
+  "Helena",
+  "Nietzche"
+]
+let ageList = [
+  66,
+  85,
+  70,
+  60,
+  50,
+  50,
+  60,
+  60,
+  45,
+  44,
+]
+let  colorList = [
+  "Azul",
+  "Amarelo",
+  "Vermelho",
+  "Verde",
+  "Rosa",
+  "Lilás",
+  "Preto",
+  "Roxo",
+  "Beje",
+  "Branco", 
+]
+
+console.log(nameList)
+console.log(ageList)
+console.log(colorList)
+
+ageList[0] = 67
+colorList[6] = "Black"
+
+
+console.log(colorList)
+console.log(ageList)
