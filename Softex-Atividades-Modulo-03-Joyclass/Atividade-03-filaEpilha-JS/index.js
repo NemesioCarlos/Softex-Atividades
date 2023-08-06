9364a3c6e66f1100a3f860f8a2f94d2e55d7518e
Nemesio carlos <114183514+NemesioCarlos@users.noreply.github.com>
2023-08-06 19:30:47 -0300

    Add files via upload

---
+++ Softex-Atividades-Modulo-03-Joyclass/Atividade-03-filaEpilha-JS/index.js
@@ -0,0 +1,15 @@
+// considerando uma fila essa seria a sequência de números removidos: [3, 7, 9, 1, 0] usando .pop
+let list = [3, 7, 9, 1, 0]
+while ( list.length > 0) { 
+  list.pop()
+}
+console.log(list)
+
+
+
+// considerando uma pilha essa seria a sequência de números removidos: [0, 1, 9, 7, 3] usando .shift()
+let list = [3, 7, 9, 1, 0]
+while ( list.length > 0 ) {
+list.shift()
+}
+console.log(list)
---
+++ Softex-Atividades-Modulo-03-Joyclass/Atividade-03-filaEpilha-JS/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let list = [3, 7, 9, 1, 0]
+while ( list.length > 0) { 
+  list.pop()
+
+}
+console.log(list)
