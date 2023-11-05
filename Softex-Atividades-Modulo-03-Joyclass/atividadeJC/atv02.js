1228768c9ef95fbe3dfb8f34fbdb10c713afb552
Nemesio carlos <114183514+NemesioCarlos@users.noreply.github.com>
2023-11-05 09:58:18 -0300

    Add files via upload

---
+++ Softex-Atividades-Modulo-03-Joyclass/atividadeJC/atv02.js
@@ -0,0 +1,8 @@
+let nota1 = prompt(Number("Digite a nota 1"));
+let nota2 = prompt(Number("Digite a nota 2"));
+let nota3 = prompt(Number("Digite a nota 3"));
+
+let soma = nota1 + nota2 + nota3;
+let media = soma / 3;
+
+console.log(media);
