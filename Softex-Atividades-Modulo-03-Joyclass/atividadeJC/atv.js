0bfe8986fce9b837bf9de67a944e438d631da87d
Nemesio carlos <114183514+NemesioCarlos@users.noreply.github.com>
2023-11-05 09:47:33 -0300

    Add files via upload

---
+++ Softex-Atividades-Modulo-03-Joyclass/atividadeJC/atv.js
@@ -0,0 +1,9 @@
+let nome = "Nemésio Carlos";
+let salario = 2300.00;
+let idade = 26;
+let diploma = true;
+
+console.log(typeof nome);
+console.log(typeof salario);
+console.log(typeof idade);
+console.log(typeof diploma);
