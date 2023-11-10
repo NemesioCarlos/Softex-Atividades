f403faf7cfd950da19a75a30ceb4ed105302751c
Nemesio carlos <114183514+NemesioCarlos@users.noreply.github.com>
2023-11-10 09:30:15 -0300

    Add files via upload

---
+++ Softex-Atividades-Modulo-03-Joyclass/atividadeJC/atv04.js
@@ -0,0 +1,17 @@
+function randomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+const numeroAleatorio = randomNumber();
+console.log(`O número aleatório gerado é: ${numeroAleatorio}`);
+
+////////////////////////////////////////
+
+function soma(n1, n2) {
+  return n1 + n2;
+}
+
+/////////////////////////////////////////
+
+const user1 = () => ({ nome: "carlos", idade: 26 });
+
